7e800d4f3187ead6f3e29225ee96509766a8007e
hyetigran <tigran.asriyan@gmail.com>
2020-04-22 21:48:19 -0400

    anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  //filter both strings of space and special chars
+  // lower case both strings
+  let strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  let strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  // base case: if strings aren't same length, return false
+  if (strA.length !== strB.length) {
+    return false;
+  }
+
+  //create empty object
+  let objA = {};
+  // loop string A and add to object
+  for (let char of strA) {
+    objA[char] ? objA[char]++ : (objA[char] = 1);
+  }
+
+  console.log(objA);
+  // convert string B to array
+  let arrB = strB.split("");
+  // loop over array
+  for (let i = 0; i < arrB.length; i++) {
+    // check if character is equal to zero
+    if (objA[arrB[i]] == 0) {
+      return false;
+    }
+    // if array A of el is a key in obj A AND if it doesn't equal zero
+    if (objA[arrB[i]] && objA[arrB[i]] !== 0) {
+      //decrement
+      objA[arrB[i]]--;
+    }
+  }
+  //if loop finishes without throwing and error, return true
+  return true;
+}
 
 module.exports = anagrams;
